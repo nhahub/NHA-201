7b20a43c4b0516e4868fe9853987300572eaf1b0
Ola Younis <olasabry16@gmail.com>
2025-12-03 03:56:05 +0200

    Update SortingFilterPage.java

--- src/main/java/SauceDemoPages/SortingFilterPage.java
+++ src/main/java/SauceDemoPages/SortingFilterPage.java
@@ -1,10 +1,8 @@
 package SauceDemoPages;
-
 import Engine.Bot.Bot;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-
 import java.util.ArrayList;
 import java.util.List;
 
