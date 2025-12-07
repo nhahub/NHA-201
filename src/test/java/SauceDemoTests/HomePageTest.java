4684536c5119973e99a6d74ddecdf7fb4adab282
nada khamis <nadakhamis803@gmail.com>
2025-12-08 00:24:12 +0200

    last version

--- src/test/java/SauceDemoTests/HomePageTest.java
+++ src/test/java/SauceDemoTests/HomePageTest.java
@@ -12,40 +12,46 @@ public class HomePageTest extends BaseTest {
 
     @BeforeMethod
     public void setUpHomePage() {
-        new LoginPage(bot).loginAsStandardUser();
-        homePage = new HomePage(bot);
+        new LoginPage(bot)
+            .loginAsStandardUser();
     }
 
     @Test
     public void Home_Tc1_scrollAndCheckBadge() {
-        homePage.scrollToBottom().scrollToTop();
+        new HomePage(bot)
+            .scrollToBottom()
+            .scrollToTop();
         Assert.assertTrue(new LoginPage(bot).getCurrentUrl().contains("inventory"),
                 "User is not on Home / Inventory page!");
     }
 
     @Test
     public void Home_Tc2_testOpenMenu() {
-        homePage.clickMenuButton();
-        Assert.assertTrue(homePage.isMenuOpen(), "Menu should be visible after clicking the button");
+        new HomePage(bot)
+            .clickMenuButton();
+        Assert.assertTrue(new HomePage(bot).isMenuOpen(),
+                "Menu should be visible after clicking the button");
     }
 
     @Test
     public void Home_Tc3_addMultipleProducts() {
-        homePage.addBackpackToCart()
-                .addBikeLightToCart()
-                .addBoltShirtToCart();
+        new HomePage(bot)
+            .addBackpackToCart()
+            .addBikeLightToCart()
+            .addBoltShirtToCart();
         //String badge = homePage.getCartBadgeCount();
         Assert.assertEquals(new HomePage(bot).getCartBadgeCount() , "3",
                 "Cart badge should be '3' after adding Multiple Products");
     }
     @Test
     public void Home_Tc4_removeProducts() {
-        homePage.addBackpackToCart()
-                .addBikeLightToCart()
-                .addBoltShirtToCart()
-                .removeBackpack()
-                .removeBikeLight()
-                .removeBoltShirt();
+        new HomePage(bot)
+            .addBackpackToCart()
+            .addBikeLightToCart()
+            .addBoltShirtToCart()
+            .removeBackpack()
+            .removeBikeLight()
+            .removeBoltShirt();
         //String badge = homePage.getCartBadgeCount();
         Assert.assertTrue(new HomePage(bot).getCartBadgeCount().isEmpty(),
                 "Cart badge should be empty after removing all items"
