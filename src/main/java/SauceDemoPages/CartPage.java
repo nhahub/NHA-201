ad8edaf459d31fe9a91621c85e0f9490662e47a1
Akram Hussen <akramhussen91@gmail.com>
2025-12-07 23:30:29 +0200

    Cart + checkout + confirmation

--- src/main/java/SauceDemoPages/CartPage.java
+++ src/main/java/SauceDemoPages/CartPage.java
@@ -7,6 +7,7 @@ public class CartPage {
 
     private final Bot bot;
 
+    // Locators
     private final By cartIcon = By.id("shopping_cart_container");
     private final By cartItems = By.className("cart_item");
     private final By firstItemName = By.xpath("(//div[@class='inventory_item_name'])[1]");
@@ -17,44 +18,20 @@ public class CartPage {
     private final By addBikeLightButton = By.id("add-to-cart-sauce-labs-bike-light");
     private final By addBoltTShirtButton = By.id("add-to-cart-sauce-labs-bolt-t-shirt");
 
+    // Constructor
     public CartPage(Bot bot) {
         this.bot = bot;
     }
 
+    // Actions
     public CartPage openCartIcon() {
         bot.click(cartIcon);
         return this;
     }
 
-    public boolean isCartPageOpened() {
-        return bot.getCurrentUrl().contains("cart.html");
-    }
-
-    public int countCartItems() {
-        return bot.countElements(cartItems);
-    }
-
-    public String getFirstItemName() {
-        return bot.getText(firstItemName);
-    }
-
     public CartPage removeFirstItemFromCart() {
         bot.click(removeFirstItemButton);
         return this;
-    }
-
-    public CartPage goToCheckout() {
-        openCartIcon();
-        clickCheckoutButton();
-        return this;
-    }
-
-    public int getCartBadgeNumber() {
-        String badgeText = bot.getText(cartBadgeNumber);
-        if (badgeText == null || badgeText.isEmpty()) {
-            return 0;
-        }
-        return Integer.parseInt(badgeText);
     }
 
     public CartPage addBackpack() {
@@ -73,10 +50,39 @@ public class CartPage {
         addBackpack();
         return goToCheckout();
     }
+
+    public CartPage goToCheckout() {
+        openCartIcon();
+        clickCheckoutButton();
+        return this;
+    }
+
+    // Checks for assertions
+    public boolean isCartPageOpened() {
+        return bot.getCurrentUrl().contains("cart.html");
+    }
+
+    public int countCartItems() {
+        return bot.countElements(cartItems);
+    }
+
+    public String getFirstItemName() {
+        return bot.getText(firstItemName);
+    }
+
+    public int getCartBadgeNumber() {
+        String badgeText = bot.getText(cartBadgeNumber);
+        if (badgeText == null || badgeText.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(badgeText);
+    }
+
     public String getCurrentUrl() {
         return bot.getCurrentUrl();
     }
-    //TODO: Navigate to CheckoutPage
+
+    // Navigation
     public CheckoutPage clickCheckoutButton() {
         bot.click(checkoutButton);
         return new CheckoutPage(bot);
--- src/test/java/SauceDemoTests/CheckoutPageTests.java
+++ src/test/java/SauceDemoTests/CheckoutPageTests.java
@@ -5,7 +5,6 @@ import DataDrivenTest.TestDataProvider;
 import SauceDemoPages.CartPage;
 import SauceDemoPages.CheckoutPage;
 import SauceDemoPages.LoginPage;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,52 +14,60 @@ public class CheckoutPageTests extends BaseTest {
 
     @BeforeMethod
     public void setUpCheckout() {
-       new LoginPage(bot)
-           .loginAsStandardUser();
-       new CartPage(bot)
-           .addBackpackAndGoToCheckout();
+        new LoginPage(bot)
+                .loginAsStandardUser();
+
+        new CartPage(bot)
+                .addBackpackAndGoToCheckout();
+
+        checkoutPage = new CheckoutPage(bot);
     }
 
     @Test(dataProvider = "checkoutValidData", dataProviderClass = TestDataProvider.class)
     public void testCheckoutWithValidData(String firstName, String lastName, String zipCode) {
-        new CheckoutPage(bot)
-            .fillFirstName(firstName)
-            .fillLastName(lastName)
-            .fillZipCode(zipCode)
-            .clickContinue();
+        checkoutPage
+                .fillFirstName(firstName)
+                .fillLastName(lastName)
+                .fillZipCode(zipCode)
+                .clickContinue();
+
         Assert.assertEquals(
-                new CheckoutPage(bot).getCurrentUrl(),
+                checkoutPage.getCurrentUrl(),
                 "https://www.saucedemo.com/checkout-step-two.html"
         );
     }
 
     @Test(dataProvider = "checkoutEmptyFirstName", dataProviderClass = TestDataProvider.class)
     public void testCheckoutWithEmptyFirstName(String firstName, String lastName, String zipCode) {
-        new CheckoutPage(bot)
-            .fillFirstName(firstName)
-            .fillLastName(lastName)
-            .fillZipCode(zipCode)
-            .clickContinue();
+        checkoutPage
+                .fillFirstName(firstName)
+                .fillLastName(lastName)
+                .fillZipCode(zipCode)
+                .clickContinue();
+
         Assert.assertNotEquals(
-                new CheckoutPage(bot).getCurrentUrl(),
+                checkoutPage.getCurrentUrl(),
                 "https://www.saucedemo.com/checkout-step-two.html"
         );
-        String errorMsg = bot.findAndGetText(By.xpath("//h3[@data-test='error']"));
+
+        String errorMsg = checkoutPage.getErrorMessageText();
         Assert.assertTrue(errorMsg.contains("Error"));
     }
 
     @Test(dataProvider = "checkoutEmptyZip", dataProviderClass = TestDataProvider.class)
     public void testCheckoutWithEmptyZip(String firstName, String lastName, String zipCode) {
-        new CheckoutPage(bot)
-           .fillFirstName(firstName)
-           .fillLastName(lastName)
-           .fillZipCode(zipCode)
-           .clickContinue();
+        checkoutPage
+                .fillFirstName(firstName)
+                .fillLastName(lastName)
+                .fillZipCode(zipCode)
+                .clickContinue();
+
         Assert.assertNotEquals(
-                new CheckoutPage(bot).getCurrentUrl(),
+                checkoutPage.getCurrentUrl(),
                 "https://www.saucedemo.com/checkout-step-two.html"
         );
-        String errorMsg = bot.findAndGetText(By.xpath("//h3[@data-test='error']"));
+
+        String errorMsg = checkoutPage.getErrorMessageText();
         Assert.assertTrue(errorMsg.contains("Error"));
     }
 }
